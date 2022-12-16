4ed3a83b94d7fa2cc8e257b5dad3edf3aa3ac50b
Rich4ever11 <rich4ever11@protonmail.com>
2022-12-16 15:50:41 -0500

    Update about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 
 export default function About() {
@@ -16,19 +17,35 @@ export default function About() {
       <div style={{ textAlign: "center" }}>
         <div>
           <p>
-            “Md Covid-19 - Cases by ZIP Code.” Catalog, Publisher
+            I got the data for covid-19 in baltimore city from the source listed
+            below. “Md Covid-19 - Cases by ZIP Code.” Catalog, Publisher
             Opendata.maryland.gov, 25 Nov. 2022,
             https://catalog.data.gov/dataset/md-covid-19-cases-by-zip-code.
           </p>
         </div>
         <div>
           <p>
-            “Part 1 Crime Data.” Open Baltimore,
+            I got the data for crime in baltimore city from the data source
+            listed below. “Part 1 Crime Data.” Open Baltimore,
             https://data.baltimorecity.gov/datasets/baltimore::part-1-crime-data-/explore?location=39.300270%2C-76.618650%2C13.00.{" "}
           </p>
         </div>
 
-        <Link to="/">Return To Dashboard</Link>
+        <Grid align="center" sx={{ m: 2 }}>
+          <div style={{ textAlign: "center" }}>
+            <Link to="/">
+              <Typography
+                component="h3"
+                variant="h4"
+                align="center"
+                color="text.primary"
+                gutterBottom
+              >
+                Return To Dashboard
+              </Typography>
+            </Link>
+          </div>
+        </Grid>
       </div>
     </div>
   );
