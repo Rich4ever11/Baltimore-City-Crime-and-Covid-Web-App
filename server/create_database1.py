0ee10baca71d0295330eb7e1b7dea1c36fdaaa20
Rich4ever11 <67285654+Rich4ever11@users.noreply.github.com>
2022-12-18 11:46:21 -0500

    Create create_database1.py

---
+++ server/create_database1.py
@@ -0,0 +1,70 @@
+from server import db, app
+from server import Crime
+from server import Covid
+import json
+import uuid
+
+with app.app_context():
+    db.create_all()
+
+    #if this doesnt work copy the full path or the relative path to these two files
+    covidFile = open('server\BaltimoreCityCovidData.json', 'r')
+    crimeFile = open('server\BaltimoreCityCrimeData.json', 'r')
+
+    crimeData = json.load(crimeFile)
+    covidData = json.load(covidFile)
+
+    for section in crimeData["features"]:
+        newCrimeID = section["properties"]["RowID"]
+        newCrimeDate = section["properties"]["CrimeDateTime"]
+        newCrimeCode = section["properties"]["CrimeCode"]
+        newCrimeLocation = section["properties"]["Location"]
+        newCrimeDescription = section["properties"]["Description"]
+        newCrimeWeapon = section["properties"]["Weapon"]
+        newCrimeGender = section["properties"]["Gender"]
+        newCrimeAge = section["properties"]["Age"]
+        newCrimeRace = section["properties"]["Race"]
+        newCrimeEthnicity = section["properties"]["Ethnicity"]
+        newCrimeDistrict = section["properties"]["District"]
+        newCrimeNeighborhood = section["properties"]["Neighborhood"]
+        newCrimeLatitude = section["properties"]["Latitude"]
+        newCrimeLongitude = section["properties"]["Longitude"]
+
+        newCrime = Crime(crimeID=newCrimeID, 
+        CrimeDateTime=newCrimeDate, 
+        CrimeCode=newCrimeCode,
+        Location=newCrimeLocation, 
+        Description = newCrimeDescription,
+        Weapon = newCrimeWeapon,
+        Gender = newCrimeGender,
+        Age = newCrimeAge,
+        Race = newCrimeRace,
+        Ethnicity = newCrimeEthnicity,
+        District = newCrimeDistrict,
+        Neighborhood = newCrimeNeighborhood,
+        Latitude = newCrimeLatitude,
+        Longitude = newCrimeLongitude
+        )
+        
+        try:
+            db.session.add(newCrime)
+            db.session.commit()
+        except:
+            db.session.rollback()
+
+    for section in covidData["covidList"]:
+        newCovidID = section["OBJECTID"]
+        newCovidDate = section["DATE"]
+        newCovidCount = section["Baltimore_City"]
+        
+        newCovid = Covid(covidID=newCovidID, 
+        covidDate=newCovidDate, 
+        covidCount=newCovidCount)
+
+        try:
+            db.session.add(newCovid)
+            db.session.commit()
+        except:
+            db.session.rollback()
+
+exit()
